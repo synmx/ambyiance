23695234c1f298d6bd993eff6233c429c1506f3c
Syntyche Mumbaya <mumbayacynthia@gmail.com>
2022-01-26 22:05:26 +0000

    created a simple header for ambyiance on react app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ambyiance</h1>
+
+      {/* Heder */}
+      {/* Swipe Card */}
+      {/* Buttons below swipe cards */}
+
+      {/* Chat screen*/}
+      {/* Private chat screen */}
+
+       {/* Quiz master */}
+       {/* select to be player or master */}
+       {/* Quiz room */}
+
+       {/* audable chatroom */}
+       {/* select topic of disscussion*/}
+       {/* audio chatroom */}
+
     </div>
   );
 }
